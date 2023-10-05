7940e148c89e97168b7e515baf667b27670573d8
Nandini <nandinikistipati1729@gmail.com>
2023-10-05 22:10:27 +0530

    flask basics done

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-# from flask import Flask,render_template,redirect,request,url_for,session
+from flask import Flask,render_template,redirect,request,url_for,session
+from model import *
+app=Flask(__name__)
+from model import db
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Blogs.sqlite3'
 
-# app=Flask(__name__)
-# app.congif
+db.init_app(app)
+app.app_context().push()
+
+@app.route('/')
+def index():
+    b=Blog.query.all()
+    return render_template('index.html',blogs=b)
+
+@app.route('/submit-blog', methods=['POST'])
+def submit_blog():
+    a = request.form['blogTitle']
+    b = request.form['blogContent']
+    blog = Blog(title = a, content=b)
+    db.session.add(blog)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete_blog/<int:blog_id>')
+def delete_blog(blog_id):
+    blog=Blog.query.get(blog_id)
+    db.session.delete(blog)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__=="__main__":
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,3 +1,7 @@
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
-# db=SQLALchemy()
+db=SQLAlchemy()
+class Blog(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    content=db.Column(db.Text,nullable=False)
